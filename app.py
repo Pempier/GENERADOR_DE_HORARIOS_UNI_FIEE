2653ff7453e2b7b2e75d2986bc68849938c08b51
Pempier <geniopempier@gmail.com>
2025-04-18 23:45:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, send_file
 import pandas as pd
 
@@ -16,4 +17,5 @@ def get_cursos():
     return jsonify(cursos_unicos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
