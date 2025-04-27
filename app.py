42f27f1f5ec0dd5ba4a4e326f1a5af28131d5604
Pempier <geniopempier@gmail.com>
2025-04-27 01:58:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     return send_file("index.html")
 
 @app.route("/api/cursos")
-def get_cursos():
+def api_cursos():
     df = pd.read_excel("HORARIOS.xlsx", skiprows=2)
     df.columns = ['ESP', 'COD', 'SECC', 'CURSO', 'DOCENTE', 'TIPO', 'CICLO', 'DIA', 'H_INI', 'H_FIN', 'SALON']
     df = df[df['CURSO'].notna()]
